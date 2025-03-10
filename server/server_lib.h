40643e0cab8d4fb187bb5ed1fe5d3f2fe37213f4
savipauk <erik.kranjec8@outlook.com>
2025-03-11 00:46:28 +0100

    Refactor server_lib.h to use std::array instead of C style array

--- server/server_lib.h
+++ server/server_lib.h
@@ -1,24 +1,25 @@
 #pragma once
 
 #include <SDL_net.h>
+#include <array>
 
 #define MAX_CLIENTS 2
 
 namespace darena {
 
 struct TCPServer {
-  bool client_connected[MAX_CLIENTS];
-  TCPsocket client_communication_socket[MAX_CLIENTS];
+  std::array<bool, MAX_CLIENTS> client_connected;
+  std::array<TCPsocket, MAX_CLIENTS> client_communication_socket;
   TCPsocket server_listening_socket;
   SDLNet_SocketSet socket_set;
   int client_id = 0;
 
   TCPServer()
       : server_listening_socket(nullptr),
-        client_communication_socket(nullptr),
         socket_set(nullptr) {
     for (int i = 0; i < MAX_CLIENTS; i++) {
       client_connected[i] = false;
+      client_communication_socket[i] = NULL;
     }
   }
 
