2fba5321645cfb99aed067e7e756938d8dfb664e
savipauk <erik.kranjec8@outlook.com>
2025-03-10 13:05:37 +0100

    Modify build.sh to add --clean and remove --link.

--- build.sh
+++ build.sh
@@ -5,12 +5,17 @@ set -x  # Print each command before executing
 mkdir -p out
 cd out
 
-rm -rf *
-cmake ../
+# Do a clean run if doing ./build.sh --clean
+if [[ $1 == "--clean" ]]; then
+  rm -rf *
+  cmake ../
+fi
 
 cmake --build .
 
 # Generate symlink to out/compile_command.json if doing `./build.sh --link`
-if [[ $1 == "--link" ]]; then
+if [ -e compile_commands.json ]; then
+  echo "Link already exists. Skipping ln -s out/compile_commands.json compile_commands.json"
+else
   ln -s out/compile_commands.json compile_commands.json
 fi
